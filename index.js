059d00b3617e5a0817e79e98c620aff51ff96bde
roxylius <98657313+roxylius@users.noreply.github.com>
2022-07-18 11:41:00 +0530

    final draft

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-document.querySelector(".de").
+function change(color) {
+    var date = new Date();
+    var day = date.getDay();
+
+    var dayNames = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+    document.querySelector("." + dayNames[day]).style.background = color;
+}
+
+change("#000");
